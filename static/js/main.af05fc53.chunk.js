1981b4ad7287aade2d2ea6aff122dbf90975c620
KashnykovAnton <kashnikov_anton@yahoo.com>
2021-10-15 00:21:22 +0300

    Updates

---
+++ static/js/main.af05fc53.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-03-image-finder"]=this["webpackJsonpgoit-react-hw-03-image-finder"]||[]).push([[0],{10:function(e,t,n){},12:function(e,t,n){"use strict";n.r(t);var c=n(1),i=n.n(c),r=n(3),o=n.n(r),a=(n(8),n(9),n(10),n(0));var s=function(){return Object(a.jsx)("div",{children:Object(a.jsx)("h1",{children:"Welcome to React!"})})};o.a.render(Object(a.jsx)(i.a.StrictMode,{children:Object(a.jsx)(s,{})}),document.getElementById("root"))},9:function(e,t,n){}},[[12,1,2]]]);
+//# sourceMappingURL=main.af05fc53.chunk.js.map
